60c716ad83fd18ab59a9456f1847f43122437c45
John Thompson <john@springframework.guru>
2019-06-07 05:08:19 -0400

    updating to junit 5. closes #2

---
+++ src/test/java/guru/sfg/beer/order/service/BeerOrderServiceTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.beer.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeerOrderServiceTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
