47e15ce7cc9cf3f9f854f9a45c094fabf4611351
John <john@springframework.guru>
2019-06-07 07:55:28 -0400

    fixing tests

--- src/test/java/guru/sfg/beer/order/service/BeerOrderServiceApplicationTests.java
+++ src/test/java/guru/sfg/beer/order/service/BeerOrderServiceApplicationTests.java
@@ -1,11 +1,8 @@
 package guru.sfg.beer.order.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BeerOrderServiceApplicationTests {
 
