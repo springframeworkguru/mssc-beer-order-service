2666553fb59373a0abc0622e33e7152a324d7fc2
John Thompson <john@springframework.guru>
2019-11-30 11:40:41 -0500

    enabling JMS Autoconfig. closes #24

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -2,13 +2,11 @@ package guru.sfg.beer.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class BeerOrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeerOrderServiceApplication.class, args);
     }
-
 }
