1002cf0ebba17f322d9a123b2efded23cff0f5b6
John Thompson <john@springframework.guru>
2020-02-19 20:37:58 -0500

    adding tests for allocation failure and partial allocation. Closes #33

--- src/test/java/guru/sfg/beer/order/service/services/testcomponets/BeerOrderAllocationListener.java
+++ src/test/java/guru/sfg/beer/order/service/services/testcomponets/BeerOrderAllocationListener.java
@@ -23,16 +23,34 @@ public class BeerOrderAllocationListener {
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
     public void listen(Message msg){
         AllocateOrderRequest request = (AllocateOrderRequest) msg.getPayload();
+        boolean pendingInventory = false;
+        boolean allocationError = false;
+
+        //set pending inventory
+        if (request.getBeerOrderDto().getCustomerRef() != null && request.getBeerOrderDto().getCustomerRef().equals("partial-allocation")){
+            pendingInventory = true;
+        }
+
+        //set allocation error
+        if (request.getBeerOrderDto().getCustomerRef() != null && request.getBeerOrderDto().getCustomerRef().equals("fail-allocation")){
+            allocationError = true;
+        }
+
+        boolean finalPendingInventory = pendingInventory;
 
         request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
-            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+            if (finalPendingInventory) {
+                beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity() - 1);
+            } else {
+                beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+            }
         });
 
         jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
                 AllocateOrderResult.builder()
                 .beerOrderDto(request.getBeerOrderDto())
-                .pendingInventory(false)
-                .allocationError(false)
+                .pendingInventory(pendingInventory)
+                .allocationError(allocationError)
                 .build());
     }
 }
