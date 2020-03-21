5c3a698b4f15f367812a84469e8a1a549e42aaa2
jeffreymzd <mzhde@hotmail.com>
2020-03-21 12:05:11 -0400

    Disable integration tests, update .gitignore to ignore target files

--- src/test/java/guru/sfg/beer/order/service/services/beer/BeerServiceImplTest.java
+++ src/test/java/guru/sfg/beer/order/service/services/beer/BeerServiceImplTest.java
@@ -1,5 +1,6 @@
 package guru.sfg.beer.order.service.services.beer;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import java.util.UUID;
 /**
  * Created by jeffreymzd on 3/19/20
  */
+@Disabled
 @SpringBootTest
 class BeerServiceImplTest {
 
