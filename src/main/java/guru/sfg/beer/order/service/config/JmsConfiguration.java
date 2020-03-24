9ca169d0ba93023a52a2a08ebd163f8ce590fa38
jeffreymzd <mzhde@hotmail.com>
2020-03-23 21:38:14 -0400

    Add JMS config

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfiguration.java
@@ -0,0 +1,24 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by jeffreymzd on 3/23/20
+ */
+@Configuration
+public class JmsConfiguration {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
