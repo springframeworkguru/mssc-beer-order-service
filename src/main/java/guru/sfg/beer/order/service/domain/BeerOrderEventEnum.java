f4ca1ef13e7c06cc3754f4988061bb88b9581e18
bcasalvieri <bmcasalvieri@gmail.com>
2020-04-14 12:32:24 -0400

    adding enums for sm

---
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
@@ -0,0 +1,7 @@
+package guru.sfg.beer.order.service.domain;
+
+public enum BeerOrderEventEnum {
+    VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED,
+    ALLOCATION_SUCCESS, ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED,
+    BEERORDER_PICKED_UP
+}
