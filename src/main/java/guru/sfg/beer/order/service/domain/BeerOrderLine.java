2c518ff76e60fe4dfbdae081b9e4bbb21aaa2e91
aminmb37 <amin.mb37@gmail.com>
2020-10-17 13:27:39 +0200

    upc property added to BeerOrderLine.

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderLine.java
@@ -33,23 +33,28 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @Entity
-public class BeerOrderLine extends BaseEntity {
-
+public class BeerOrderLine extends BaseEntity
+{
     @Builder
-    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
-                         Integer quantityAllocated) {
+    public BeerOrderLine(
+        UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
+        BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity,
+        Integer quantityAllocated)
+    {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
+
 
     @ManyToOne
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
 }
---
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl_.java
@@ -0,0 +1,67 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.domain.BeerOrderLine.BeerOrderLineBuilder;
+import guru.sfg.brewery.model.BeerOrderLineDto;
+import guru.sfg.brewery.model.BeerOrderLineDto.BeerOrderLineDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-10-15T22:51:28+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
+)
+@Component
+@Qualifier("delegate")
+public class BeerOrderLineMapperImpl_ implements BeerOrderLineMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line) {
+        if ( line == null ) {
+            return null;
+        }
+
+        BeerOrderLineDtoBuilder beerOrderLineDto = BeerOrderLineDto.builder();
+
+        beerOrderLineDto.id( line.getId() );
+        if ( line.getVersion() != null ) {
+            beerOrderLineDto.version( line.getVersion().intValue() );
+        }
+        beerOrderLineDto.createdDate( dateMapper.asOffsetDateTime( line.getCreatedDate() ) );
+        beerOrderLineDto.lastModifiedDate( dateMapper.asOffsetDateTime( line.getLastModifiedDate() ) );
+        beerOrderLineDto.upc( line.getUpc() );
+        beerOrderLineDto.beerId( line.getBeerId() );
+        beerOrderLineDto.orderQuantity( line.getOrderQuantity() );
+        beerOrderLineDto.quantityAllocated( line.getQuantityAllocated() );
+
+        return beerOrderLineDto.build();
+    }
+
+    @Override
+    public BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BeerOrderLineBuilder beerOrderLine = BeerOrderLine.builder();
+
+        beerOrderLine.id( dto.getId() );
+        if ( dto.getVersion() != null ) {
+            beerOrderLine.version( dto.getVersion().longValue() );
+        }
+        beerOrderLine.createdDate( dateMapper.asTimestamp( dto.getCreatedDate() ) );
+        beerOrderLine.lastModifiedDate( dateMapper.asTimestamp( dto.getLastModifiedDate() ) );
+        beerOrderLine.beerId( dto.getBeerId() );
+        beerOrderLine.upc( dto.getUpc() );
+        beerOrderLine.orderQuantity( dto.getOrderQuantity() );
+        beerOrderLine.quantityAllocated( dto.getQuantityAllocated() );
+
+        return beerOrderLine.build();
+    }
+}
